9d6e183baa75546860c71f7d239516d7434da8cb
justinpham2004 <phamjustin04@gmail.com>
2024-06-25 13:33:31 -0400

    finished part3

--- neural_network/nn.py
+++ neural_network/nn.py
@@ -54,11 +54,11 @@ def generate_classification_report_test(true_labels, predicted_labels, val_class
     df = pd.DataFrame(report).transpose()
 
     # Add class Names
-    new_class_names = val_class_names[:]
+    new_class_names = val_class_names
     if len(val_class_names) < len(df.index):
         new_class_names.extend([''] * (len(df.index) - len(new_class_names)))
-    df.insert(0, 'sample_labels', new_class_names)
-    df = df.reset_index().rename(columns={'index': 'sample_labels'})
+    df.insert(0, 'category_labels', new_class_names)
+    df = df.reset_index().rename(columns={'index': 'category_labels'})
     df.to_csv(output_name)
 
 """
@@ -71,13 +71,35 @@ def generate_classification_report_test(true_labels, predicted_labels, val_class
         returns (list of label_names, list of encoded labels)
 
 """
-def encode_labels(label_type, df): 
-    labels = df[f'{label_type}_label']
+def encode_labels(label_type, df, test_df): 
+    ## Variables needed for label mapping ##    
+    test_labels = None
+    labels = None
+    test_label_map = None
+   
+    if label_type == 'sample':
+        test_labels = test_df['sample_label'] 
+        labels = df['sample_label']
+    elif label_type == 'category':
+        test_labels = test_df['category_label'] ##These are just the full columns of data from the NN
+        labels = df['category_label']
+    else:
+        raise ValueError("Enter a Valid Label Type!")
 
-    encoded_labels, labelmap = getLabelEncodedy(labels)
-    label_names = list(labelmap)
-    encoded_testset = [labelmap[label] for label in labels]
-    return (label_names, encoded_testset)
+    test_label_map = getLabelEncodedy(test_labels)[1] 
+
+    matched_labels = dict()
+    unmatched_labels = set()
+    for label in labels:
+        if label in test_label_map:
+            matched_labels[label] = test_label_map[label]
+        else:
+            matched_labels[label] = 999
+
+    encoded_set = [matched_labels[label] for label in labels]
+    print(len(unmatched_labels))
+
+    return (test_label_map, encoded_set)
 
 def get_label(neural_network):
     split = neural_network.split("_")
@@ -87,6 +109,8 @@ def get_label(neural_network):
             return word
     raise ValueError("Label Type Not Found: Specify what label is being searched for in neural network name. Ex: 'labeltype_neural_network'")
 
+
+
 """ Command Line Arguments:
     # 1: script name
     # 2: CSV with TIDS and metadata
@@ -94,35 +118,53 @@ def get_label(neural_network):
     # 4: Predicted clusters csv
     """
 def main():
-    """
+    
+    
     if len(sys.argv) != 4:
         raise ValueError("Use Format: python  nn.py  <input_clusters>.csv  <neural_network_name.h5> <nn_predictions>.csv")
-    """
-    #args = sys.argv[1:]
-    #input = args[0]
-    #neural_network = args[1]
-    #output = args[2]
-    input = 'data/nn_data.csv'
-    neural_network = '/models/xinadata2021v2-NoTIC30_Model1Dap2022_category_7'
-    output = 'output/practice.csv'
+    
+    args = sys.argv[1:]
+    input = args[0]
+    neural_network = args[1]
+    output = args[2]
+    training_data = './data/modified_updated_alldata2023-TIC30filtered.csv'
 
+    
+    """
+    ## Scripts for testing ##
+    input = './data/nn_data.csv'
+    neural_network = './models/modified_updated_alldata2023-TIC30filtered_Model1Dap2022_category_7.h5'
+    #neural_network = './models/modified_updated_alldata2023-TIC30filtered_Model1Dap2022_sample_7.h5'
+    output = './output/practice_category.csv'
+    training_data = './data/modified_updated_alldata2023-TIC30filtered.csv'
+    """
     
     """
     Get label name from nn_name:
     """
     label_type = get_label(neural_network)
-    
    
     """
     Testfile Data Housekeeping
     - Drop columns 1050-2000 as those are empty MOMA data. Note: Using df.loc() drops a slice of labels!
     - Drop mcalDefault: all data entrys just come out to 1
     """
-    full_datafile = pd.read_csv(input)
+    try:
+        full_datafile = pd.read_csv(input)
+    except Exception as e:
+        print(f"Error reading input file: {e}")
+        sys.exit(1)
+    
     datafile = full_datafile.drop(full_datafile.loc[:,'1050':'2000'].columns, axis = 1)
     datafile.drop('mcalDefault', axis = 1, inplace = True)
+    datafile.drop('cluster_number', axis = 1, inplace = True)
 
-
+    try:
+        test_datafile = pd.read_csv(training_data)
+    except Exception as e:
+        print(f"Error reading training data file: {e}")
+        sys.exit(1)
+    
     """
     Data Preprocessing Step:
     1. Read in massspec data as a df.slice
@@ -139,22 +181,25 @@ def main():
     ##Step 1, 2: Separating Meta Data and Mass Spec
     mass_spec_data = datafile.iloc[:,3:-10] ## Columns 3 to -10, only data
     #mass_spec_params = datafile.iloc[:,-10:-3] ## All columns up to -10 # In case you want parameter data
+    #print(mass_spec_data.head) #(For Testing purposes)#
 
     ## Step 3: Normalization of relevant data
     ms_normedMax = mass_spec_data.apply(lambda x: x/x.max(), axis = 1)
-    #params_normedMax = mass_spec_params.apply(lambda x: x/x.max(), axis = 1) ## Isnt really used in Victorias Code?
 
-    ## Step 4: Label Encoding
-    label_names, encoded_testset = encode_labels(label_type, full_datafile)
+    ## Step 4: Label Encoding: By matching labels from the testing data used for these NN, the NN can correctly label the prediction to the correct label#   
+    label_set, encoded_testset = encode_labels(label_type, full_datafile, test_datafile) 
 
-    ## NN for Sample Labels:
+    ## NN for Sample Labels: Loading the model and predicting the data
     loaded_model = tf.keras.models.load_model(neural_network, compile = True)
     predictions_loaded_model = loaded_model.predict(ms_normedMax)
     predictions = np.argmax(predictions_loaded_model, axis = 1)
-    
-    ## Generate Output
-    generate_classification_report_test(encoded_testset, predictions, label_names, label_type, output)
 
+    ## Generate Output
+    generate_classification_report_test(encoded_testset, predictions, label_set, output)
+    
+    ## These are sample data to be used for testing ##
+    #generate_classification_report_test(category_encoded_testset, predictions, category_labels, "category_label_test.csv")
+    #generate_classification_report_test(sample_encoded_testset, predictions, sample_labels, "sample_label_test.csv")
 
 
 if __name__ == "__main__":
